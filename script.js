1ad55f6fb18c2535d89c76ac003a37d45fe07202
Blessing Francis <blessingngf20@gmail.com>
2026-02-19 09:38:25 +0000

    Add files via upload
    Here is the summary of how I have experienced different places and cultures through tasting their food.

---
+++ script.js
@@ -0,0 +1,47 @@
+body
+{
+  font-family: system-ui;
+  background:  pink;
+  color: brown;
+  text-align: center;}
+  
+  
+  p{color: green;
+  background-color: yellow;}
+  
+
+h2{
+  color: purple;}
+
+h3{
+  button{
+    background-color: violet;
+  color: brown;
+  border: none;
+  height: 35px;
+  width: 120px;
+  border-radius: 4px;}
+  h4{
+    color: brown;}
+  }
+
+h5{
+  color: white;
+  }
+  
+
+h6{
+  color: green;}
+  
+  
+
+    font-size:100%;
+#kebab-heading{
+  color:green;}
+#kebab{
+  color: green;
+  background-color: orange;
+}
+.why-i-love-food{
+  background-color: violet;
+}
